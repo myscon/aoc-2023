9e654207e539edcf35b0a6096ef6d5bc859e4d46
myscon <myscontruong@gmail.com>
2023-12-04 01:44:17 -0800

    completed day 4

--- answers/2023/day04.rs
+++ answers/2023/day04.rs
@@ -2,7 +2,7 @@
 //! DAY:    04
 
 use crate::prelude::*;
-use std::io::BufRead;
+use std::{collections::VecDeque, io::BufRead};
 
 impl Answers for Day {
     fn new(input: PathBuf) -> Self {
@@ -14,21 +14,80 @@ impl Answers for Day {
         return BufReader::new(file);
     }
 
-    fn part_one(&self) -> Result<String, Box<dyn Error>>  {
+    fn part_one(&self) -> Result<String, Box<dyn Error>> {
         let reader = self.read();
+        let mut aggreg = 0;
+        let base: i32 = 2;
         for line in reader.lines() {
             let read_line = line.unwrap();
-        // Answer logic here
+            // I refuse to iterate char by char its too late at night
+            let card_split = read_line.split(": ").collect::<Vec<&str>>()[1];
+            let num_split = card_split.split("|").collect::<Vec<&str>>();
+            let win_split = num_split[0].split_whitespace().collect::<Vec<&str>>();
+            let lot_split = num_split[1].split_whitespace().collect::<Vec<&str>>();
+            let mut count = 0;
+            for num in lot_split {
+                if win_split.contains(&num) {
+                    count += 1;
+                }
+            }
+            let card_score;
+            if count > 0 {
+                card_score = 1 * base.pow(count - 1);
+            } else {
+                card_score = 0;
+            }
+            aggreg += card_score
         }
-        Ok("".into())
+        Ok(aggreg.to_string())
     }
 
     fn part_two(&self) -> Result<String, Box<dyn Error>> {
         let reader = self.read();
+        let mut aggreg = 0;
+        let mut card_acc = VecDeque::new();
         for line in reader.lines() {
             let read_line = line.unwrap();
-        // Answer logic here
+            // Maybe I should have made something better...
+            let card_split = read_line.split(": ").collect::<Vec<&str>>()[1];
+            let num_split = card_split.split("|").collect::<Vec<&str>>();
+            let win_split = num_split[0].split_whitespace().collect::<Vec<&str>>();
+            let lot_split = num_split[1].split_whitespace().collect::<Vec<&str>>();
+            let mut count = 0;
+            for num in lot_split {
+                if win_split.contains(&num) {
+                    count += 1;
+                }
+            }
+            let copies = card_acc.pop_front().unwrap_or(0);
+            let base = 1 + copies;
+            // please don't laugh at me...
+            if count > 0 {
+                if card_acc.len() == 0 {
+                    for _i in 0..count {
+                        card_acc.push_back(base)
+                    }
+                } else if card_acc.len() >= count {
+                    let mut i = 0;
+                    // so VecDeque apparently doesn't support random access and indexing is O(n)
+                    for card in card_acc.iter_mut() {
+                        *card += base;
+                        i += 1;
+                        if i == count {
+                            break;
+                        }
+                    }
+                } else if card_acc.len() < count {
+                    for card in card_acc.iter_mut() {
+                        *card += base;
+                    }
+                    for _i in 0..count - card_acc.len() {
+                        card_acc.push_back(base);
+                    }
+                }
+            }
+            aggreg += base;
         }
-        Ok("".into())
+        Ok(aggreg.to_string())
     }
 }
--- src/answer.rs
+++ src/answer.rs
@@ -1,15 +1,8 @@
 //! YEAR:   2023
-//! DAY:    03
+//! DAY:    04
 
 use crate::prelude::*;
-use fancy_regex::Regex;
 use std::{collections::VecDeque, io::BufRead};
-
-lazy_static! {
-    static ref SYMBOL: Regex = Regex::new(r"[^\d.]").unwrap();
-    static ref NUMBER: Regex = Regex::new(r"\d+").unwrap();
-    static ref GEAR: Regex = Regex::new(r"\*").unwrap();
-}
 
 impl Answers for Day {
     fn new(input: PathBuf) -> Self {
@@ -22,87 +15,79 @@ impl Answers for Day {
     }
 
     fn part_one(&self) -> Result<String, Box<dyn Error>> {
-        let mut reader = self.read();
+        let reader = self.read();
         let mut aggreg = 0;
-        let mut targets = VecDeque::new();
-        targets.push_back("".to_string());
-        for _i in 0..2 {
-            enqueue_line(&mut targets, &mut reader);
-        }
-        let mut num_matches = NUMBER.find_iter(&targets[1]);
-        while !targets[1].is_empty() {
-            for num in &mut num_matches {
-                let num = num.unwrap();
-                let start = num.start().saturating_sub(1);
-                let end = num.end().saturating_add(1).min(140);
-                if symbol_search(&targets, start, end) {
-                    aggreg += num.as_str().parse::<i32>().unwrap()
+        let base: i32 = 2;
+        for line in reader.lines() {
+            let read_line = line.unwrap();
+            // I refuse to iterate char by char its too late at night
+            let card_split = read_line.split(": ").collect::<Vec<&str>>()[1];
+            let num_split = card_split.split("|").collect::<Vec<&str>>();
+            let win_split = num_split[0].split_whitespace().collect::<Vec<&str>>();
+            let lot_split = num_split[1].split_whitespace().collect::<Vec<&str>>();
+            let mut count = 0;
+            for num in lot_split {
+                if win_split.contains(&num) {
+                    count += 1;
                 }
             }
-            let _ = targets.pop_front();
-            enqueue_line(&mut targets, &mut reader);
-            num_matches = NUMBER.find_iter(&targets[1]);
+            let card_score;
+            if count > 0 {
+                card_score = 1 * base.pow(count - 1);
+            } else {
+                card_score = 0;
+            }
+            aggreg += card_score
         }
         Ok(aggreg.to_string())
     }
 
     fn part_two(&self) -> Result<String, Box<dyn Error>> {
-        let mut reader = self.read();
+        let reader = self.read();
         let mut aggreg = 0;
-        let mut targets = VecDeque::new();
-        targets.push_back("".to_string());
-        for _i in 0..2 {
-            enqueue_line(&mut targets, &mut reader);
-        }
-        let mut num_matches = GEAR.find_iter(&targets[1]);
-        while !targets[1].is_empty() {
-            for num in &mut num_matches {
-                let num = num.unwrap();
-                let start = num.start().saturating_sub(3);
-                let end = num.end().saturating_add(3).min(140);
-                aggreg += number_search(&targets, start, end);
+        let mut card_acc = VecDeque::new();
+        for line in reader.lines() {
+            let read_line = line.unwrap();
+            // Maybe I should have made something better...
+            let card_split = read_line.split(": ").collect::<Vec<&str>>()[1];
+            let num_split = card_split.split("|").collect::<Vec<&str>>();
+            let win_split = num_split[0].split_whitespace().collect::<Vec<&str>>();
+            let lot_split = num_split[1].split_whitespace().collect::<Vec<&str>>();
+            let mut count = 0;
+            for num in lot_split {
+                if win_split.contains(&num) {
+                    count += 1;
+                }
             }
-            let _ = targets.pop_front();
-            enqueue_line(&mut targets, &mut reader);
-            num_matches = GEAR.find_iter(&targets[1]);
+            let copies = card_acc.pop_front().unwrap_or(0);
+            let base = 1 + copies;
+            // please don't laugh at me...
+            if count > 0 {
+                if card_acc.len() == 0 {
+                    for _i in 0..count {
+                        card_acc.push_back(base)
+                    }
+                } else if card_acc.len() >= count {
+                    let mut i = 0;
+                    // so VecDeque apparently doesn't support random access and indexing is O(n)
+                    for card in card_acc.iter_mut() {
+                        *card += base;
+                        i += 1;
+                        if i == count {
+                            break;
+                        }
+                    }
+                } else if card_acc.len() < count {
+                    for card in card_acc.iter_mut() {
+                        *card += base;
+                    }
+                    for _i in 0..count - card_acc.len() {
+                        card_acc.push_back(base);
+                    }
+                }
+            }
+            aggreg += base;
         }
         Ok(aggreg.to_string())
     }
-}
-
-fn number_search(targets: &VecDeque<String>, start: usize, end: usize) -> i32 {
-    let mut nums = vec!();
-    for target in targets {
-        if !target.is_empty() {
-            let num_matches = NUMBER.find_iter(&target[start..end]);
-            for num_match in num_matches {
-                let num_res = num_match.unwrap();
-                let st = num_res.start();
-                let ed = num_res.end();
-                if st < 5 && ed > 2 {
-                    nums.push(num_res.as_str())
-                }
-            }
-        }
-    }
-    if nums.len() == 2{
-        return nums[0].parse::<i32>().unwrap()*nums[1].parse::<i32>().unwrap()
-    } else {
-        return 0;
-    }
-}
-
-fn symbol_search(targets: &VecDeque<String>, start: usize, end: usize) -> bool {
-    for target in targets {
-        if !target.is_empty() && SYMBOL.find(&target[start..end]).unwrap().is_some() {
-            return true;
-        }
-    }
-    return false;
-}
-
-fn enqueue_line(queue: &mut VecDeque<String>, reader: &mut BufReader<File>) {
-    let mut texts = String::new();
-    let _ = reader.read_line(&mut texts);
-    queue.push_back(texts);
 }
