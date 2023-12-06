63ca1fcd7b30a59de5486cb6314bd7d54971aa1e
myscon <myscontruong@gmail.com>
2023-12-06 13:07:37 -0800

    completed day 6

--- src/day.rs
+++ src/day.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 
 pub trait Answers {
     fn new(input: PathBuf) -> Self;
-    fn read(&self) -> BufReader<File>;
+    fn reader(&self) -> BufReader<File>;
     fn part_one(&self) -> Result<String, Box<dyn Error>>;
     fn part_two(&self) -> Result<String, Box<dyn Error>>;
 }
